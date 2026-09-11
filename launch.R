e3600bfc0fcfa6b6a33b686b545718b24b046583
weaversd <sweaver4@nd.edu>
2026-09-11 16:05:56 -0400

    added user lib

--- launch.R
+++ launch.R
@@ -1,4 +1,10 @@
-# launch.R
+user_lib <- Sys.getenv("R_LIBS_USER")
+if (!dir.exists(user_lib)) {
+  dir.create(user_lib, recursive = TRUE, showWarnings = FALSE)
+}
+.libPaths(c(user_lib, .libPaths()))
+
+
 if (!requireNamespace("shiny", quietly = TRUE)) {
   install.packages("shiny", repos = "https://cloud.r-project.org", type = "binary")
 }
